ba3aebf1da92b450188308bb234840c800cf183e
Gfliess <Gfliess@10.211.55.15>
2013-04-09 00:57:34 +0200

    add empty starter

---
+++ Performance/src/at/edu/hti/concurrency/starter/EmptyStarter.java
@@ -0,0 +1,18 @@
+ package at.edu.hti.concurrency.starter;
+
+import java.util.List;
+
+import at.edu.hti.concurrency.Consumer;
+import at.edu.hti.concurrency.Producer;
+import at.edu.hti.concurrency.Starter;
+import at.edu.hti.concurrency.Store;
+
+public class EmptyStarter implements Starter{
+
+	@Override
+	public void run(Store store, List<Producer> p, List<Consumer> c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
