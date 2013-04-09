b637701b33374e2e1dad592212fa8ddebf9fd43f
ctrummer <c.trummer@ssi-schaefer-peem.com>
2013-04-09 19:12:01 +0200

    Using GIT ...

--- Performance/src/at/edu/hti/concurrency/stores/StoreImpl.java
+++ Performance/src/at/edu/hti/concurrency/stores/StoreImpl.java
@@ -1,76 +1,78 @@
 package at.edu.hti.concurrency.stores;
 
 import java.util.LinkedList;
-import java.util.Queue;
 
 import at.edu.hti.concurrency.Store;
 import at.edu.hti.concurrency.StoreEventListener;
 
+/**
+ * Store implementation with LinkedList
+ */
 public class StoreImpl implements Store {
-	
-	private LinkedList<String> queue = new LinkedList<String>(); 
 
-	@Override
-	public String getName() {	
-		return "StoreImpl";
-	}
+    private LinkedList<String> queue = new LinkedList<String>();
 
-	@Override
-	public void initMaxSize(int maxSize) {
-		// TODO Auto-generated method stub
+    @Override
+    public String getName() {
+        return "StoreImpl";
+    }
 
-	}
+    @Override
+    public void initMaxSize(int maxSize) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void add(String data) {
-		queue.addFirst(data);
+    }
 
-	}
+    @Override
+    public void add(String data) {
+        queue.addFirst(data);
 
-	@Override
-	public String remove() {
-		return queue.removeLast();
-		
-	}
+    }
 
-	@Override
-	public String removeItem(int index) {
-		return queue.remove(index);
-	}
+    @Override
+    public String remove() {
+        return queue.removeLast();
 
-	@Override
-	public int size() {
-		return queue.size();
-	}
+    }
 
-	@Override
-	public void registerListener(StoreEventListener listener) {
-		// TODO Auto-generated method stub
+    @Override
+    public String removeItem(int index) {
+        return queue.remove(index);
+    }
 
-	}
+    @Override
+    public int size() {
+        return queue.size();
+    }
 
-	@Override
-	public boolean active() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public void registerListener(StoreEventListener listener) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void shutDown() {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    @Override
+    public boolean active() {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public void incProducer() {
-		// TODO Auto-generated method stub
+    @Override
+    public void shutDown() {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public void decProducer() {
-		// TODO Auto-generated method stub
+    @Override
+    public void incProducer() {
+        // TODO Auto-generated method stub
 
-	}
+    }
+
+    @Override
+    public void decProducer() {
+        // TODO Auto-generated method stub
+
+    }
 
 }
